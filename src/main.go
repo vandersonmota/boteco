3c1a36fbc55dbd2dd70800779c775aa2e7571a90
Vanderson Mota <vanderson.mota@gmail.com>
2020-06-11 23:41:47 +0200

    signal handling stuff

--- src/main.go
+++ src/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"os/signal"
+)
 
+func main() {
+	log.Println("PotatoMQ daemon started")
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig)
+	<-sig
 }

5d51462f4a5e6864d79232487965f6a7f9e62a0f
Vanderson Mota <vanderson.mota@gmail.com>
2020-06-13 14:45:39 +0200

    initial server implementation with cleanup

--- src/main.go
+++ src/main.go
@@ -4,11 +4,14 @@ import (
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/vandersonmota/potatomq/src/server"
 )
 
 func main() {
 	log.Println("PotatoMQ daemon started")
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig)
-	<-sig
+	s := server.NewTCPServer("tcp", ":9876", sig)
+	s.Listen()
 }
